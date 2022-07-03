5d9a09806236729223260cdf7329817533d369f5
selvakumar.s88@gmail.com <selvakumar.s88@gmail.com>
2022-07-03 05:56:04 +0530

    Junit initial

---
+++ src/test/java/org/project/Test.java
@@ -0,0 +1,24 @@
+package org.project;
+
+public class Test {
+public @interface before {
+
+	}
+
+@org.junit.Test  
+@before
+
+	public void add()
+	
+	{
+		System.out.println("hi");
+	}
+
+
+
+	
+
+	
+	
+	
+}
